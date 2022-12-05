710309c4c9479404c391e4ab893fbad58155a17d
takeshi tsuruda <lazywayofcat@gmail.com>
2022-12-05 20:51:12 +0900

    HTMLを設定

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,6 @@
 module PostsHelper
   def choose_new_or_edit
-    if action_name == 'new' || action_name == 'create'
+    if action_name == 'new' || action_name == 'create' || action_name =='show'
       confirm_posts_path
     elsif action_name == 'edit'
       post_path
